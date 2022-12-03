1082fd07045841469b3505df2b979303e75ed486
Henrik <headwiki@pm.me>
2022-12-03 12:35:34 +0100

    Part 2

--- src/main.rs
+++ src/main.rs
@@ -6,20 +6,31 @@ fn main() -> io::Result<()> {
     let reader = BufReader::new(file);
 
     let mut most_calories = 0;
+    let mut second_most_calories = 0;
+    let mut third_most_calories = 0;
     let mut accumulated_calories = 0;
     
 
     for line in reader.lines() {
         let moved_line = line?;
         if moved_line == "" {
-            if accumulated_calories > most_calories { most_calories = accumulated_calories; }
+            if accumulated_calories >= most_calories { 
+                third_most_calories = second_most_calories;
+                second_most_calories = most_calories;
+                most_calories = accumulated_calories; 
+            } else if accumulated_calories >= second_most_calories {
+                third_most_calories = second_most_calories;
+                second_most_calories = accumulated_calories;
+            } else if accumulated_calories > third_most_calories {
+                third_most_calories = accumulated_calories;
+            }
             accumulated_calories = 0;
         } else {
             accumulated_calories = accumulated_calories + moved_line.parse::<u32>().unwrap();
         }
     }
 
-    println!("{}", most_calories);
+    println!("{}", most_calories + second_most_calories + third_most_calories);
 
     Ok(())
 }
